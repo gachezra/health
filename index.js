29a36289672ed68575ecc041c08bf6ace075c1c6
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-05-13 00:15:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { startHealthCheckTimer, receiveHealthCheck } = require('health');
+
+app.use(cors());
+app.use(express.json());
+
+// Health check endpoint
+app.get('/on', receiveHealthCheck);
+
+app.listen(port, () => {
+  console.log("Server Started on Port", port);
+  // Start the health check timer after server starts
+  startHealthCheckTimer();
+});
